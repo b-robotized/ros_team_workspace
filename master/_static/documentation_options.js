c59dddd6eaa2e39bafeb8299d5f0cbb2bde8855e
destogl <destogl@users.noreply.github.com>
2025-08-11 17:53:31 +0000

    deploy: 605cb657a5858080095b22321e4297b1c7875fa3

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Jul 2025',
+    VERSION: 'Aug 2025',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
