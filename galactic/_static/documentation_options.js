fa06c27d7a5afb77292d0ba7798fe10ad5155105
destogl <destogl@users.noreply.github.com>
2025-09-12 11:33:14 +0000

    deploy: f1762c1518e4bceae99f8956afc16d710176158f

--- galactic/_static/documentation_options.js
+++ galactic/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Aug 2025',
+    VERSION: 'Sep 2025',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
