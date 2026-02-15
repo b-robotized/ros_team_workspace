c2675c91eed50145af9e3357f1c8cc03cb10dfd4
Dr. Denis <denis.stogl@b-robotized.com>
2026-02-15 15:14:03 +0100

    Extend script to run all tests locally

--- tests/run_docker_tests.bash
+++ tests/run_docker_tests.bash
@@ -1,3 +1,15 @@
 #!/bin/bash
+set -e
+
 docker build --pull --no-cache -t rtw_test -f tests/Dockerfile.test .
-docker run --rm rtw_test
+
+echo "================================================================"
+echo "Running RTW CLI tests..."
+echo "================================================================"
+docker run --rm rtw_test ./tests/run_rtw_cli_tests.bash
+
+echo ""
+echo "================================================================"
+echo "Running package creation and bringup tests..."
+echo "================================================================"
+docker run --rm rtw_test ./tests/run_tests.bash
