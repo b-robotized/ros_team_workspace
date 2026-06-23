d565136eadb5a4d43fd600932840be3ebb86b095
Yara Shahin <yara.shahin@b-robotized.com>
2026-06-23 13:20:16 +0300

    update license header

--- templates/testing/test_package_name.launch.py
+++ templates/testing/test_package_name.launch.py
@@ -1,14 +1,18 @@
-# Copyright (c) 2025 b»robotized
-# All rights reserved.
+#!/bin/bash
 #
-# Proprietary License
+# Copyright (c) 2021-2026, b»robotized group
 #
-# Unauthorized copying of this file, via any medium is strictly prohibited.
-# The file is considered confidential
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Adapted for <Insert_Company_Name> that received unlimited, worldwide
-# use and change right, except distributing this library separately
-# of their product.
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import unittest
 import rclpy
